1af6f35ca83a43747f9958148f7adc81bb61b2e6
Kang <vancong171001@gmail.com>
2025-07-06 14:21:32 +0700

    C3 - V3: Build a to-do list

--- Todo App/src/App.jsx
+++ Todo App/src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
 
 function App() {
-  return <p>Todo App</p>;
+  return (
+    <div>
+      <input type="text" name="add-new-task" placeholder="Add new task" />
+      <div>
+        <p>Đi học thêm</p>
+      </div>
+      <div>
+        <p>Học võ</p>
+      </div>
+      <div>
+        <p>Học bài</p>
+      </div>
+      <div>
+        <p>Đi chợ</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
