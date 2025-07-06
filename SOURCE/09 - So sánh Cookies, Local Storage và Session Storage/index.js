bbe650c2ff39364e38adb375ebf2bde56f54a6eb
Kang <vancong171001@gmail.com>
2025-07-06 11:57:19 +0700

    C2 - V9: Comparing Cookies, Local Storage and Session Storage

---
+++ SOURCE/09 - So sánh Cookies, Local Storage và Session Storage/index.js
@@ -0,0 +1,18 @@
+// document.cookie = `name=John; expires=${new Date(
+//   '2025-07-07 10:00:00'
+// ).toUTCString()}`;
+// document.cookie = `age=30; expires=${new Date(
+//   '2025-07-07 10:00:00'
+// ).toUTCString()}`;
+
+// console.log(document.cookie);
+
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', 30);
+
+// console.log(localStorage.getItem('name'));
+// localStorage.removeItem('age');
+// localStorage.clear();
+
+// sessionStorage.setItem('job', 'Developer');
+// sessionStorage.setItem('age', 30);
