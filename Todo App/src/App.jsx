78ad3496d294517e7469928e4b42698e303f6516
Kang <vancong171001@gmail.com>
2025-07-08 16:27:55 +0700

    C4 - V14: Solving JavaScript Variable Problems Using State

--- Todo App/src/App.jsx
+++ Todo App/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
 function App() {
-  const todoList = [
+  const [todoList, setTodoList] = useState([
     { id: 1, name: 'Đi học thêm' },
     { id: 2, name: 'Đi học võ' },
     { id: 3, name: 'Làm bài' },
-  ];
+  ]);
+
+  console.log({ todoList, setTodoList });
+
+  // const todoList = [
+  //   { id: 1, name: 'Đi học thêm' },
+  //   { id: 2, name: 'Đi học võ' },
+  //   { id: 3, name: 'Làm bài' },
+  // ];
 
   const todos = todoList.map(todo => {
     return <TodoItem name={todo.name} key={todo.id} />;
@@ -20,6 +29,16 @@ function App() {
         name="add-new-task"
         placeholder="Add new task"
         className="task-input"
+        onKeyDown={e => {
+          if (e.key === 'Enter') {
+            const value = e.target.value;
+            console.log(value);
+            setTodoList([
+              ...todoList,
+              { id: crypto.randomUUID(), name: value },
+            ]);
+          }
+        }}
       />
       <div>{todos}</div>
     </div>
