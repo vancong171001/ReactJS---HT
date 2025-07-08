7e8642d566733e017c20e8e11c63c2f778224cc6
Kang <vancong171001@gmail.com>
2025-07-08 15:59:27 +0700

    C4 - V11: Event Handling in React

--- Todo App/src/components/TodoItem.jsx
+++ Todo App/src/components/TodoItem.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const TodoItem = props => {
   console.log(props);
+
+  function handleClick() {
+    alert(props.name);
+  }
+
   return (
-    <div className="todo-item">
+    <div className="todo-item" onClick={handleClick}>
       <p className="todo-item-text">{props.name}</p>
     </div>
   );
